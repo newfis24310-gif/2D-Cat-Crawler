350eb13cd98f39e1e68b0d40751cfa0fb21ac9d9
Newfis <newfis24310@gmail.com>
2026-03-18 18:50:52 +0200

    FMOD test

---
+++ Assets/Scripts/Scriptable/SFXLibrary.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using FMODUnity;
+
+[CreateAssetMenu(menuName = "Audio/SFX Library")]
+public class SFXLibrary : ScriptableObject
+{
+    public EventReference boxOpen;
+    public EventReference gas;
+}
