b2553f28efcdcadcc83f9f7539e2a7d21ee9ee32
Nikoleta Makri <mkrnikoleta@gmail.com>
2026-03-20 14:21:01 +0200

    το χαζογατο καταλαβαινει οτι βρηκε item

---
+++ Assets/Scripts/Items/FishItem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class FishItem : Item
+{
+
+    public override void OnInteract(Player player)
+    {
+        Debug.Log("The cat found the fish");
+        // Λογικη θα προστεθει αργοτερα
+    }
+}
